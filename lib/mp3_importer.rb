34396959f66d4674228ad0206523ffa3729036d4
Brad McCollum <btmcco8@gmail.com>
2018-08-22 20:43:07 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,12 +2,18 @@ class MP3Importer
 
   attr_accessor :path
 
+  @@files = []
+
   def initialize(path) #initializes with a file path
     @path = path
   end
 
   def files
     Dir.entries("./db/mp3s").each {|x| puts x}
+  end
+
+  def self.files
+    @@files
   end
 
   def import
