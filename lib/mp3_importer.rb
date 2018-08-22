7d65674f915755627a91daa98a1f10ea381582f9
Brad McCollum <btmcco8@gmail.com>
2018-08-22 20:53:35 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,7 @@
 class MP3Importer
-
   attr_accessor :path
 
-  @@files = []
-
-  def initialize(path) #initializes with a file path
+  def initialize(path)
     @path = path
   end
 
@@ -12,13 +9,8 @@ class MP3Importer
     Dir.entries("./spec/fixtures/mp3s").select {|song_filename| song_filename.include?("mp3")}
   end
 
-  def self.files
-    @@files
-  end
-
   def import
-    binding.pry
-    Song.new_by_filename(files.each {|x| x})
+    files.each {|song_filename| Song.new_by_filename(song_filename)}
   end
 
 end
