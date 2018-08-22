8b89d6497ae8f4bac7cec5446f16640809e910e1
Brad McCollum <btmcco8@gmail.com>
2018-08-22 20:50:37 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,9 +9,7 @@ class MP3Importer
   end
 
   def files
-    # @@files << Dir.entries("./db/mp3s").each {|x| "#{x}"}
-    # binding.pry
-    Dir.entries("./spec/fixtures/mp3s").select {|x| x.include?("mp3")}
+    Dir.entries("./spec/fixtures/mp3s").select {|song_filename| song_filename.include?("mp3")}
   end
 
   def self.files
