671797581547d9d0f3983a638fa9ac342c626ab5
Brad McCollum <btmcco8@gmail.com>
2018-08-22 20:39:32 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path) #initializes with a file path 
+    @path = path
+  end
+
+  def files
+    Dir.entries("./db/mp3s").each {|x| Song.new_by_filename(x)}
+  end
+
+  def import
+    
+  end
+
+end
