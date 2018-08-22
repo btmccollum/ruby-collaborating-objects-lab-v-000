43a6cf5c49aaa39aae78a7a1d315b6786435192c
Brad McCollum <btmcco8@gmail.com>
2018-08-22 20:06:25 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,41 @@
+require 'pry'
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    self.save
+  end
+
+  def self.find_or_create_by_name(artist)
+    if @@all.any? {|x| x.name == artist}
+      "#{artist} already found."
+    else
+      Artist.new(artist)
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def print_songs
+    @songs.each {|x| x.name}
+  end
+
+  def save
+    self.class.all << self
+  end
+end
+# binding.pry
---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    # binding.pry
+    @name = name
+    self.name
+  end
+
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+
+  def self.new_by_filename(filename)
+    # binding.pry
+    song = self.new(filename.split(/[-\.]/)[1].strip)
+    song.artist_name = (filename.split(/[-\.]/)[0].strip)
+    song.artist.add_song(self)
+    song.save
+    song
+  end
+end
